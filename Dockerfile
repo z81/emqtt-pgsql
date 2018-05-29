dd5943f4ddd46eb52e34cbb27b1283d7150ee35e
z81 <muzo0@yandex.ru>
2018-05-29 17:31:09 +0300

    fix

--- Dockerfile
+++ Dockerfile
@@ -1,110 +1,11 @@
-FROM alpine:3.7
+FROM emqttd-docker-v2.0.6:latest
+ADD ./start.sh /opt/emqttd/start.sh
+ADD ./table.sql /opt/emqttd/table.sql
 
-MAINTAINER Huang Rui <vowstar@gmail.com>, Turtle <turtled@emqtt.io>
-
-ENV EMQ_VERSION=v2.3.9
-
-RUN set -ex \
-    # add build deps, remove after build
-    && apk --no-cache add --virtual .build-deps \
-        build-base \
-        # gcc \
-        # make \
-        bsd-compat-headers \
-        perl \
-        erlang \
-        erlang-public-key \
-        erlang-syntax-tools \
-        erlang-erl-docgen \
-        #erlang-gs \
-        erlang-observer \
-        erlang-ssh \
-        #erlang-ose \
-        erlang-cosfiletransfer \
-        erlang-runtime-tools \
-        erlang-os-mon \
-        erlang-tools \
-        erlang-cosproperty \
-        erlang-common-test \
-        erlang-dialyzer \
-        erlang-edoc \
-        erlang-otp-mibs \
-        erlang-crypto \
-        erlang-costransaction \
-        erlang-odbc \
-        erlang-inets \
-        erlang-asn1 \
-        erlang-snmp \
-        erlang-erts \
-        erlang-et \
-        erlang-cosnotification \
-        erlang-xmerl \
-        #erlang-typer \
-        erlang-coseventdomain \
-        erlang-stdlib \
-        erlang-diameter \
-        erlang-hipe \
-        erlang-ic \
-        erlang-eunit \
-        #erlang-webtool \
-        erlang-mnesia \
-        erlang-erl-interface \
-        #erlang-test-server \
-        erlang-sasl \
-        erlang-jinterface \
-        erlang-kernel \
-        erlang-orber \
-        erlang-costime \
-        #erlang-percept \
-        erlang-dev \
-        erlang-eldap \
-        erlang-reltool \
-        erlang-debugger \
-        erlang-ssl \
-        erlang-megaco \
-        erlang-parsetools \
-        erlang-cosevent \
-        erlang-compiler \
-    # add fetch deps, remove after build
-    && apk add --no-cache --virtual .fetch-deps \
-        git \
-        wget \
-    # add run deps, never remove
-    && apk add --no-cache --virtual .run-deps \
-        ncurses-terminfo-base \
-        ncurses-terminfo \
-        ncurses-libs \
-        readline \
-    # add latest rebar
-    && git clone -b ${EMQ_VERSION} https://github.com/emqtt/emq-relx.git /emqttd_g \
-    && cd /emqttd_g \
-    && make \
-    && mkdir -p /emqttd && mv /emqttd_g/_rel/emqttd /emqttd \
-    && cd / && rm -rf /emqttd_g \
-    && chmod +x /emqttd/start.sh \
-    && ln -s /emqttd/bin/* /usr/local/bin/ \
-    # removing fetch deps and build deps
-    && apk --purge del .build-deps .fetch-deps \
-    && rm -rf /var/cache/apk/*
-
-WORKDIR /emqttd
-
-RUN adduser -D -u 1000 emqtt
-
-RUN chgrp -Rf root /emqttd && chmod -Rf g+w /emqttd \
-      && chown -Rf emqtt /emqttd
-
-USER emqtt
-
-VOLUME ["/emqttd/log", "/emqttd/data", "/emqttd/lib", "/emqttd/etc"]
-
-ADD ./start.sh /emqttd/start.sh
-RUN chmod +x /emqttd/start.sh
-
-ADD ./table.sql /emqttd/table.sql
+WORKDIR /opt/emqttd
 
 # start emqttd and initial environments
-CMD ["/emqttd/start.sh"]
+CMD ["/opt/emqttd/start.sh"]
 
 # emqttd will occupy these port:
 # - 1883 port for MQTT
--- start.sh
+++ start.sh
@@ -1,10 +1,9 @@
-
 #!/bin/sh
 ## EMQ docker image start script
 # Huang Rui <vowstar@gmail.com>
 
 ## EMQ Base settings
-# Base settings in /emqttd/etc/emq.conf
+# Base settings in /opt/emqttd/etc/emq.conf
 
 if [ x"${EMQ_NAME}" = x ]
 then
@@ -23,133 +22,133 @@ then
 EMQ_NODE_NAME="${EMQ_NAME}@${EMQ_HOST}"
 echo "EMQ_NODE_NAME=${EMQ_NODE_NAME}"
 fi
-sed -i -e "s/^#*\s*node.name\s*=\s*.*@.*/node.name = ${EMQ_NODE_NAME}/g" /emqttd/etc/emq.conf
+sed -i -e "s/^#*\s*node.name\s*=\s*.*@.*/node.name = ${EMQ_NODE_NAME}/g" /opt/emqttd/etc/emq.conf
 
 if [ x"${EMQ_NODE_COOKIE}" = x ]
 then
 EMQ_NODE_COOKIE="emq_dist_cookie"
 echo "EMQ_NODE_COOKIE=${EMQ_NODE_COOKIE}"
 fi
-sed -i -e "s/^#*\s*node.cookie\s*=\s*.*/node.cookie = ${EMQ_NODE_COOKIE}/g" /emqttd/etc/emq.conf
+sed -i -e "s/^#*\s*node.cookie\s*=\s*.*/node.cookie = ${EMQ_NODE_COOKIE}/g" /opt/emqttd/etc/emq.conf
 
 if [ x"${EMQ_PROCESS_LIMIT}" = x ]
 then
 EMQ_PROCESS_LIMIT=2097152
 echo "EMQ_PROCESS_LIMIT=${EMQ_PROCESS_LIMIT}"
 fi
-sed -i -e "s/^#*\s*node.process_limit\s*=\s*.*/node.process_limit = ${EMQ_PROCESS_LIMIT}/g" /emqttd/etc/emq.conf
+sed -i -e "s/^#*\s*node.process_limit\s*=\s*.*/node.process_limit = ${EMQ_PROCESS_LIMIT}/g" /opt/emqttd/etc/emq.conf
 
 if [ x"${EMQ_MAX_PORTS}" = x ]
 then
 EMQ_MAX_PORTS=1048576
 echo "EMQ_MAX_PORTS=${EMQ_MAX_PORTS}"
 fi
-sed -i -e "s/^#*\s*node.max_ports\s*=\s*.*/node.max_ports = ${EMQ_MAX_PORTS}/g" /emqttd/etc/emq.conf
+sed -i -e "s/^#*\s*node.max_ports\s*=\s*.*/node.max_ports = ${EMQ_MAX_PORTS}/g" /opt/emqttd/etc/emq.conf
 
 if [ x"${EMQ_LOG_CONSOLE}" = x ]
 then
 EMQ_LOG_CONSOLE="file"
 echo "EMQ_LOG_CONSOLE=${EMQ_LOG_CONSOLE}"
 fi
-sed -i -e "s/^#*\s*log.console\s*=\s*.*/log.console = ${EMQ_LOG_CONSOLE}/g" /emqttd/etc/emq.conf
+sed -i -e "s/^#*\s*log.console\s*=\s*.*/log.console = ${EMQ_LOG_CONSOLE}/g" /opt/emqttd/etc/emq.conf
 
 if [ x"${EMQ_LOG_LEVEL}" = x ]
 then
 EMQ_LOG_LEVEL="error"
 echo "EMQ_LOG_LEVEL=${EMQ_LOG_LEVEL}"
 fi
-sed -i -e "s/^#*\s*log.console.level\s*=\s*.*/log.console.level = ${EMQ_LOG_LEVEL}/g" /emqttd/etc/emq.conf
+sed -i -e "s/^#*\s*log.console.level\s*=\s*.*/log.console.level = ${EMQ_LOG_LEVEL}/g" /opt/emqttd/etc/emq.conf
 
 if [ x"${EMQ_ALLOW_ANONYMOUS}" = x ]
 then
 EMQ_ALLOW_ANONYMOUS="false"
 echo "EMQ_ALLOW_ANONYMOUS=${EMQ_ALLOW_ANONYMOUS}"
 fi
-sed -i -e "s/^#*\s*mqtt.allow_anonymous\s*=\s*.*/mqtt.allow_anonymous = ${EMQ_ALLOW_ANONYMOUS}/g" /emqttd/etc/emq.conf
+sed -i -e "s/^#*\s*mqtt.allow_anonymous\s*=\s*.*/mqtt.allow_anonymous = ${EMQ_ALLOW_ANONYMOUS}/g" /opt/emqttd/etc/emq.conf
 
 if [ x"${EMQ_TCP_PORT}" = x ]
 then
 EMQ_TCP_PORT=1883
 echo "EMQ_TCP_PORT=${EMQ_TCP_PORT}"
 fi
-sed -i -e "s/^#*\s*mqtt.listener.tcp\s*=\s*.*/mqtt.listener.tcp = ${EMQ_TCP_PORT}/g" /emqttd/etc/emq.conf
+sed -i -e "s/^#*\s*mqtt.listener.tcp\s*=\s*.*/mqtt.listener.tcp = ${EMQ_TCP_PORT}/g" /opt/emqttd/etc/emq.conf
 
 if [ x"${EMQ_TCP_ACCEPTORS}" = x ]
 then
 EMQ_TCP_ACCEPTORS=64
 echo "EMQ_TCP_ACCEPTORS=${EMQ_TCP_ACCEPTORS}"
 fi
-sed -i -e "s/^#*\s*mqtt.listener.tcp.acceptors\s*=\s*.*/mqtt.listener.tcp.acceptors = ${EMQ_TCP_ACCEPTORS}/g" /emqttd/etc/emq.conf
+sed -i -e "s/^#*\s*mqtt.listener.tcp.acceptors\s*=\s*.*/mqtt.listener.tcp.acceptors = ${EMQ_TCP_ACCEPTORS}/g" /opt/emqttd/etc/emq.conf
 
 if [ x"${EMQ_TCP_MAX_CLIENTS}" = x ]
 then
 EMQ_TCP_MAX_CLIENTS=1000000
 echo "EMQ_TCP_MAX_CLIENTS=${EMQ_TCP_MAX_CLIENTS}"
 fi
-sed -i -e "s/^#*\s*mqtt.listener.tcp.max_clients\s*=\s*.*/mqtt.listener.tcp.max_clients = ${EMQ_TCP_MAX_CLIENTS}/g" /emqttd/etc/emq.conf
+sed -i -e "s/^#*\s*mqtt.listener.tcp.max_clients\s*=\s*.*/mqtt.listener.tcp.max_clients = ${EMQ_TCP_MAX_CLIENTS}/g" /opt/emqttd/etc/emq.conf
 
 if [ x"${EMQ_SSL_PORT}" = x ]
 then
 EMQ_SSL_PORT=8883
 echo "EMQ_SSL_PORT=${EMQ_SSL_PORT}"
 fi
-sed -i -e "s/^#*\s*mqtt.listener.ssl\s*=\s*.*/mqtt.listener.ssl = ${EMQ_SSL_PORT}/g" /emqttd/etc/emq.conf
+sed -i -e "s/^#*\s*mqtt.listener.ssl\s*=\s*.*/mqtt.listener.ssl = ${EMQ_SSL_PORT}/g" /opt/emqttd/etc/emq.conf
 
 if [ x"${EMQ_SSL_ACCEPTORS}" = x ]
 then
 EMQ_SSL_ACCEPTORS=32
 echo "EMQ_SSL_ACCEPTORS=${EMQ_SSL_ACCEPTORS}"
 fi
-sed -i -e "s/^#*\s*mqtt.listener.ssl.acceptors\s*=\s*.*/mqtt.listener.ssl.acceptors = ${EMQ_SSL_ACCEPTORS}/g" /emqttd/etc/emq.conf
+sed -i -e "s/^#*\s*mqtt.listener.ssl.acceptors\s*=\s*.*/mqtt.listener.ssl.acceptors = ${EMQ_SSL_ACCEPTORS}/g" /opt/emqttd/etc/emq.conf
 
 if [ x"${EMQ_SSL_MAX_CLIENTS}" = x ]
 then
 EMQ_SSL_MAX_CLIENTS=500000
 echo "EMQ_SSL_MAX_CLIENTS=${EMQ_SSL_MAX_CLIENTS}"
 fi
-sed -i -e "s/^#*\s*mqtt.listener.ssl.max_clients\s*=\s*.*/mqtt.listener.ssl.max_clients = ${EMQ_SSL_MAX_CLIENTS}/g" /emqttd/etc/emq.conf
+sed -i -e "s/^#*\s*mqtt.listener.ssl.max_clients\s*=\s*.*/mqtt.listener.ssl.max_clients = ${EMQ_SSL_MAX_CLIENTS}/g" /opt/emqttd/etc/emq.conf
 
 if [ x"${EMQ_HTTP_PORT}" = x ]
 then
 EMQ_HTTP_PORT=8083
 echo "EMQ_HTTP_PORT=${EMQ_HTTP_PORT}"
 fi
-sed -i -e "s/^#*\s*mqtt.listener.http\s*=\s*.*/mqtt.listener.http = ${EMQ_HTTP_PORT}/g" /emqttd/etc/emq.conf
+sed -i -e "s/^#*\s*mqtt.listener.http\s*=\s*.*/mqtt.listener.http = ${EMQ_HTTP_PORT}/g" /opt/emqttd/etc/emq.conf
 
 if [ x"${EMQ_HTTP_ACCEPTORS}" = x ]
 then
 EMQ_HTTP_ACCEPTORS=64
 echo "EMQ_HTTP_ACCEPTORS=${EMQ_HTTP_ACCEPTORS}"
 fi
-sed -i -e "s/^#*\s*mqtt.listener.http.acceptors\s*=\s*.*/mqtt.listener.http.acceptors = ${EMQ_HTTP_ACCEPTORS}/g" /emqttd/etc/emq.conf
+sed -i -e "s/^#*\s*mqtt.listener.http.acceptors\s*=\s*.*/mqtt.listener.http.acceptors = ${EMQ_HTTP_ACCEPTORS}/g" /opt/emqttd/etc/emq.conf
 
 if [ x"${EMQ_HTTP_MAX_CLIENTS}" = x ]
 then
 EMQ_HTTP_MAX_CLIENTS=1000000
 echo "EMQ_HTTP_MAX_CLIENTS=${EMQ_HTTP_MAX_CLIENTS}"
 fi
-sed -i -e "s/^#*\s*mqtt.listener.http.max_clients\s*=\s*.*/mqtt.listener.http.max_clients = ${EMQ_HTTP_MAX_CLIENTS}/g" /emqttd/etc/emq.conf
+sed -i -e "s/^#*\s*mqtt.listener.http.max_clients\s*=\s*.*/mqtt.listener.http.max_clients = ${EMQ_HTTP_MAX_CLIENTS}/g" /opt/emqttd/etc/emq.conf
 
 if [ x"${EMQ_HTTPS_PORT}" = x ]
 then
 EMQ_HTTPS_PORT=8084
 echo "EMQ_HTTPS_PORT=${EMQ_HTTPS_PORT}"
 fi
-sed -i -e "s/^#*\s*mqtt.listener.https\s*=\s*.*/mqtt.listener.https = ${EMQ_HTTPS_PORT}/g" /emqttd/etc/emq.conf
+sed -i -e "s/^#*\s*mqtt.listener.https\s*=\s*.*/mqtt.listener.https = ${EMQ_HTTPS_PORT}/g" /opt/emqttd/etc/emq.conf
 
 if [ x"${EMQ_HTTPS_ACCEPTORS}" = x ]
 then
 EMQ_HTTPS_ACCEPTORS=32
 echo "EMQ_HTTPS_ACCEPTORS=${EMQ_HTTPS_ACCEPTORS}"
 fi
-sed -i -e "s/^#*\s*mqtt.listener.https.acceptors\s*=\s*.*/mqtt.listener.https.acceptors = ${EMQ_HTTPS_ACCEPTORS}/g" /emqttd/etc/emq.conf
+sed -i -e "s/^#*\s*mqtt.listener.https.acceptors\s*=\s*.*/mqtt.listener.https.acceptors = ${EMQ_HTTPS_ACCEPTORS}/g" /opt/emqttd/etc/emq.conf
 
 if [ x"${EMQ_HTTPS_MAX_CLIENTS}" = x ]
 then
 EMQ_HTTPS_MAX_CLIENTS=1000000
 echo "EMQ_HTTPS_MAX_CLIENTS=${EMQ_HTTPS_MAX_CLIENTS}"
 fi
-sed -i -e "s/^#*\s*mqtt.listener.https.max_clients\s*=\s*.*/mqtt.listener.https.max_clients = ${EMQ_HTTPS_MAX_CLIENTS}/g" /emqttd/etc/emq.conf
+sed -i -e "s/^#*\s*mqtt.listener.https.max_clients\s*=\s*.*/mqtt.listener.https.max_clients = ${EMQ_HTTPS_MAX_CLIENTS}/g" /opt/emqttd/etc/emq.conf
 
 
 if [ x"${EMQ_MAX_PACKET_SIZE}" = x ]
@@ -157,7 +156,7 @@ then
 EMQ_MAX_PACKET_SIZE="64KB"
 echo "EMQ_MAX_PACKET_SIZE=${EMQ_MAX_PACKET_SIZE}"
 fi
-sed -i -e "s/^#*\s*mqtt.max_packet_size\s*=\s*.*/mqtt.max_packet_size = ${EMQ_MAX_PACKET_SIZE}/g" /emqttd/etc/emq.conf
+sed -i -e "s/^#*\s*mqtt.max_packet_size\s*=\s*.*/mqtt.max_packet_size = ${EMQ_MAX_PACKET_SIZE}/g" /opt/emqttd/etc/emq.conf
 
 ## EMQ Plugin load settings
 # Plugins loaded by default
@@ -169,7 +168,7 @@ echo "EMQ_LOADED_PLUGINS=${EMQ_LOADED_PLUGINS}"
 fi
 # First, remove special char at header
 # Next, replace special char to ".\n" to fit emq loaded_plugins format
-echo $(echo "${EMQ_LOADED_PLUGINS}."|sed -e "s/^[^A-Za-z0-9_]\{1,\}//g"|sed -e "s/[^A-Za-z0-9_]\{1,\}/\.\n/g") > /emqttd/data/loaded_plugins
+echo $(echo "${EMQ_LOADED_PLUGINS}."|sed -e "s/^[^A-Za-z0-9_]\{1,\}//g"|sed -e "s/[^A-Za-z0-9_]\{1,\}/\.\n/g") > /opt/emqttd/data/loaded_plugins
 
 ## EMQ Plugins setting
 
@@ -202,9 +201,9 @@ sed -e "
     s/^#*\s*auth.pgsql.auth_query\s*=\s*.*/auth.pgsql.auth_query = ${EMQ_AUTH_PGSQL_AUTHQUERY}/g;
     s/^#*\s*auth.pgsql.acl_query\s*=\s*.*/auth.pgsql.acl_query = ${EMQ_AUTH_PGSQL_ACLQUERY}/g;
     s/^#*\s*auth.pgsql.super_query\s*=\s*.*/auth.pgsql.super_query = ${EMQ_AUTH_PGSQL_SUPERQUERY}/g;
-    s/^#*\s*auth.pgsql.ssl\s*=\s*.*/auth.pgsql.ssl = ${EMQ_AUTH_PGSQL_SSL}/g" -i /emqttd/etc/plugins/emq_auth_pgsql.conf
+    s/^#*\s*auth.pgsql.ssl\s*=\s*.*/auth.pgsql.ssl = ${EMQ_AUTH_PGSQL_SSL}/g" -i /opt/emqttd/etc/plugins/emq_auth_pgsql.conf
 
-cat /emqttd/etc/plugins/emq_auth_pgsql.conf
+cat /opt/emqttd/etc/plugins/emq_auth_pgsql.conf
 ## EMQ Main script
 # Start and run emqttd, and when emqttd crashed, this container will stop
 
@@ -213,11 +212,11 @@ apk add --no-cache postgresql
 port=$(echo $EMQ_AUTH_PGSQL_SERVER | awk -F ':' '{print $2}')
 PGPASSWORD=$EMQ_AUTH_PGSQL_PASSWORD psql --port $port --file table.sql --host ${EMQ_AUTH_PGSQL_SERVER%:*} --username $EMQ_AUTH_PGSQL_USERNAME --dbname $EMQ_AUTH_PGSQL_DATABASE
 
-/emqttd/bin/emqttd start
+/opt/emqttd/bin/emqttd start
 
 # wait and ensure emqttd status is running
 WAIT_TIME=0
-while [ x$(/emqttd/bin/emqttd_ctl status |grep 'is running'|awk '{print $1}') = x ]
+while [ x$(/opt/emqttd/bin/emqttd_ctl status |grep 'is running'|awk '{print $1}') = x ]
 do
     sleep 1
     echo '['$(date -u +"%Y-%m-%dT%H:%M:%SZ")']:waiting emqttd'
@@ -236,7 +235,7 @@ echo '['$(date -u +"%Y-%m-%dT%H:%M:%SZ")']:emqttd start'
 #          you must let user know emqtt crashed and stop this container,
 #          and docker dispatching system can known and restart this container.
 IDLE_TIME=0
-while [ x$(/emqttd/bin/emqttd_ctl status |grep 'is running'|awk '{print $1}') != x ]
+while [ x$(/opt/emqttd/bin/emqttd_ctl status |grep 'is running'|awk '{print $1}') != x ]
 do  
     IDLE_TIME=`expr ${IDLE_TIME} + 1`
     echo '['$(date -u +"%Y-%m-%dT%H:%M:%SZ")']:emqttd running'
@@ -244,6 +243,6 @@ do
 done
 
 
-tail $(ls /emqttd/log/*)
+tail $(ls /opt/emqttd/log/*)
 
 echo '['$(date -u +"%Y-%m-%dT%H:%M:%SZ")']:emqttd stop'
